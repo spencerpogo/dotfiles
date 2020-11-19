616f281e0439aa84d513e0740c83be9d6a5b74f3
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2020-11-19 15:54:51 -0800

    Relax python unbound checking

--- scripts/programs/python/install.sh
+++ scripts/programs/python/install.sh
@@ -19,12 +19,10 @@ fi
 if [ ! -d ~/.pyenv/versions/3.7.7 ]; then
   pyenv install 3.7.7
 fi
-set -u
 
 # Global python packages, always run to keep it fresh
 echo "Installing python packages..."
 pip install --upgrade requests flask aiohttp black numpy scipy pandas jupyter pwntools
 
-set +u
 pyenv local 3.8.6 3.7.7
 set -u
