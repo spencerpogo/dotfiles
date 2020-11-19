d3155db31c0df24641c6ba30ac8def2650625c27
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2020-11-19 15:40:22 -0800

    Fix mispelled function name

--- scripts/programs/multimc/install.sh
+++ scripts/programs/multimc/install.sh
@@ -3,6 +3,6 @@
 if [ ! -d ~/.local/share/multimc ]; then
   echo "Installing multimc..."
   wget -O ~/Downloads/multimc.deb https://files.multimc.org/downloads/multimc_1.4-1.deb
-  aptinstall multimc.deb
+  aptinst multimc.deb
   rm ~/Downloads/multimc.deb
 fi
