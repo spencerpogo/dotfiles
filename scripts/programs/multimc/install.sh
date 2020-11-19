f08c4b1d56eb52eee710d610369600a17e3ee337
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2020-11-19 15:41:40 -0800

    Fix path

--- scripts/programs/multimc/install.sh
+++ scripts/programs/multimc/install.sh
@@ -3,6 +3,6 @@
 if [ ! -d ~/.local/share/multimc ]; then
   echo "Installing multimc..."
   wget -O ~/Downloads/multimc.deb https://files.multimc.org/downloads/multimc_1.4-1.deb
-  aptinst multimc.deb
+  aptinst ~/Downloads/multimc.deb
   rm ~/Downloads/multimc.deb
 fi
