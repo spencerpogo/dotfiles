55139ed1b32b11d7acf21545abbcd781949e2965
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2020-11-19 15:47:17 -0800

    Always load nvm

--- scripts/programs/node/install.sh
+++ scripts/programs/node/install.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+export NVM_DIR="$HOME/.nvm"
+
 if [ ! -d ~/.nvm ]; then
   echo "📦 Installing nvm"
-  export NVM_DIR="$HOME/.nvm" && (
+  (
     git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
     cd "$NVM_DIR"
     git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
-  ) && \. "$NVM_DIR/nvm.sh"
+  )
 fi
+
+. "$NVM_DIR/nvm.sh"
 
 # Always run this to stay fresh
 nvm install node
