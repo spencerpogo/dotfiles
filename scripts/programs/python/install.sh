5a2f7b054aaf9c7cf622aa645d07b86963d9c723
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2020-11-19 15:53:55 -0800

    Don't check for unbound in pyenv

--- scripts/programs/python/install.sh
+++ scripts/programs/python/install.sh
@@ -11,6 +11,7 @@ export PATH="$HOME/.pyenv/bin:$PATH"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 
+set +u
 echo "🐍 Building python versions..."
 if [ ! -d ~/.pyenv/versions/3.8.6 ]; then
   pyenv install 3.8.6
@@ -18,9 +19,12 @@ fi
 if [ ! -d ~/.pyenv/versions/3.7.7 ]; then
   pyenv install 3.7.7
 fi
+set -u
 
 # Global python packages, always run to keep it fresh
 echo "Installing python packages..."
 pip install --upgrade requests flask aiohttp black numpy scipy pandas jupyter pwntools
 
+set +u
 pyenv local 3.8.6 3.7.7
+set -u
