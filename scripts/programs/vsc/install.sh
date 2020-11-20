5dd66583226970d8d6e48ffcea1cc87f8f2fc5dc
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2020-11-20 09:28:30 -0800

    Download vsix extensions manually

--- scripts/programs/vsc/install.sh
+++ scripts/programs/vsc/install.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
+installvscext () {
+  echo "Finding version for $1..."
+  version=$(curl -s https://marketplace.visualstudio.com/items\?itemName\=EliverLara.andromeda | sed -ne 's/^.*"version":[ ]*"\([^"]*\)".*$/\1/p')
+  echo "Got version: $version"
+  # This is just terrible
+  path=$(echo "$1./vsextensions/" | awk -F '.' '{ print $1 $3 $2 }')
+  url="https://marketplace.visualstudio.com/_apis/public/gallery/publishers/$path/$version/vspackage"
+  vsix="~/Downloads/$1.vsix"
+
+  echo "Downloading $url..."
+  wget -O "$vsix" "$url"
+  echo "Installing..."
+  codium --install-extension "$vsix" --force
+}
+
 echo "Installing extensions..."
 set +e
-<~/.config/VSCodium/User/extensions.txt xargs -d '\n' -I {} codium --install-extension {} --force
+<~/.config/VSCodium/User/extensions.txt xargs -n1 installvscext
 set -e
