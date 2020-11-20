f8efcabf8de1347c6dc256f2a1e9b710f0c14d53
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2020-11-19 16:02:33 -0800

    Run local before pip

--- scripts/programs/python/install.sh
+++ scripts/programs/python/install.sh
@@ -20,9 +20,10 @@ if [ ! -d ~/.pyenv/versions/3.7.7 ]; then
   pyenv install 3.7.7
 fi
 
+pyenv local 3.8.6 3.7.7
+
 # Global python packages, always run to keep it fresh
 echo "Installing python packages..."
 pip install --upgrade requests flask aiohttp black numpy scipy pandas jupyter pwntools
 
-pyenv local 3.8.6 3.7.7
 set -u
