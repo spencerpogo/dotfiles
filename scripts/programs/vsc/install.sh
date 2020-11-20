f4b839d1042cb05acb625860989c611849211c0c
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2020-11-19 16:07:10 -0800

    Use absolute path to install extensions

--- scripts/programs/vsc/install.sh
+++ scripts/programs/vsc/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Installing extensions..."
-<../.config/VSCodium/User/extensions.txt xargs -d '\n' -I {} codium --install-extension {} --force
+<~/.config/VSCodium/User/extensions.txt xargs -d '\n' -I {} codium --install-extension {} --force
