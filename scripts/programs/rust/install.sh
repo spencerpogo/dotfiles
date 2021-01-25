e99b3238468d1b1fd67d3810300d702bbff879dd
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-01-25 10:25:08 -0800

    Add rust crates

--- scripts/programs/rust/install.sh
+++ scripts/programs/rust/install.sh
@@ -4,4 +4,10 @@ if [ ! $(command -v rustup) ]; then
   # Install rustup unattended
   log "Installing rustup..."
   curl https://sh.rustup.rs -sSf | sh -s -- -y
+  export PATH="$HOME/.cargo/bin:$PATH"
 fi
+
+log "Updating rust toolchain..."
+rustup update stable
+log "Installing rust crates..."
+cargo install cargo-tarpaulin bottom
