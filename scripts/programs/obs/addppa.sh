e0b902f3b2eae3ccc8e5be8e58068f38577dc5f1
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-02-05 16:05:49 -0800

    Remove unnecessary needsppa

--- scripts/programs/obs/addppa.sh
+++ scripts/programs/obs/addppa.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-if [ $(needpkg obs-studio) ]; then
-  log "Adding OBS PPA..."
-  addrepo obs "deb http://ppa.launchpad.net/obsproject/obs-studio/ubuntu $(codename) main"
-fi
+addrepo obs "deb http://ppa.launchpad.net/obsproject/obs-studio/ubuntu $(codename) main"
