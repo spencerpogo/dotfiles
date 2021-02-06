4325ec35bd0776d7cec1825ce71e324bb529ccf5
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-02-05 16:28:08 -0800

    Remove user option when using sudo

--- scripts/programs/flatpaks/install.sh
+++ scripts/programs/flatpaks/install.sh
@@ -7,7 +7,7 @@ sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub
 installflatpak () {
   log "Installing flatpak:" "$1"
   #set -e
-  sudo flatpak install -y --user flathub "$1"
+  sudo flatpak install -y flathub "$1"
   # install .desktop file, if it exists.
   # (just in case flatpak isn't in XDG_DATA_DIRS). 
   # On my system, I have /etc/profile.d/flatpak.sh
