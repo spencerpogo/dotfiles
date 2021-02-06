118bda29fbeb91028ffadad251b7a9efce89659b
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-02-05 16:02:00 -0800

    Remove chromium-browser package
    it is only available on ubuntu and just points to the chromium snap. Will replace with flatpak later

--- scripts/programs.sh
+++ scripts/programs.sh
@@ -49,7 +49,7 @@ install libffi-dev liblzma-dev openssl python-openssl gnupg gettext cmake
 # install apt-transport-https
 
 # Various programs (wrapping to obey max line length of 88)
-install zsh gdb chrome-gnome-shell chromium-browser dialog exfat-utils file htop nmap
+install zsh gdb chrome-gnome-shell dialog exfat-utils file htop nmap
 install openvpn tree vim wget git-lfs ncdu wine winetricks golang xxd bat gimp flatpak
 install jpegoptim optipng openjdk-11-jre openjdk-11-jdk obs-studio gnome-tweaks xclip
 install brave-browser libimage-exiftool-perl docker-ce docker-ce-cli containerd.io jq
