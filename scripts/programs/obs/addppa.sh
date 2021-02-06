0cd923f6e129a307296df0b15cd19e502b50473a
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-02-05 16:10:29 -0800

    Add OBS package key

--- scripts/programs/obs/addppa.sh
+++ scripts/programs/obs/addppa.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+addobskey () {
+  log "Adding OBS key..."
+  sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key BC7345F522079769F5BBE987EFC71127F425E228
+}
+
 addrepo obs "deb http://ppa.launchpad.net/obsproject/obs-studio/ubuntu $(codename) main"
