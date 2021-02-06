131616fe27a7668d01dfbb589ea3a53f2777db81
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-02-05 16:52:16 -0800

    Add option to disable GNOME restart
    on parrot it is much more intrusive as it completely logs the user out, killing the script process, so this is neccessary to allow the install to finish

--- scripts/programs/gnome/install.sh
+++ scripts/programs/gnome/install.sh
@@ -5,18 +5,23 @@ gnome-extensions show dash-to-panel@jderose9.github.com >/dev/null
 r=$?
 set -e
 
+restart () {
+  if [[ -z "${NO_GNOME_RESTART}" ]]; then
+    log "Restarting gnome..."
+    busctl --user call "org.gnome.Shell" "/org/gnome/Shell" "org.gnome.Shell" "Eval" "s" 'Meta.restart("Restarting…")'
+  fi
+}
+
 if [ $r -ne 0 ]; then
   log "Installing dash-to-panel..."
   git clone https://github.com/home-sweet-gnome/dash-to-panel.git panel
   cd panel && make install
   cd ..
   rm -rf panel
-  log "Restarting gnome..."
-  busctl --user call "org.gnome.Shell" "/org/gnome/Shell" "org.gnome.Shell" "Eval" "s" 'Meta.restart("Restarting…")'
+  restart
   gnome-extensions enable dash-to-panel
 fi
 
 log "Loading dconf settings..."
 dconf load /org/gnome/ < .config/dconf/settings.dconf
-log "Restarting gnome..."
-busctl --user call "org.gnome.Shell" "/org/gnome/Shell" "org.gnome.Shell" "Eval" "s" 'Meta.restart("Restarting…")'
+restart
