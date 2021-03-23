0b72b4a85c2a9a72a335865f53623d540e092d69
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-03-22 17:35:59 -0700

    Remove uneeded virtualbox PPA
    The APT packages virtualbox and virtualbox-dkms are already in the multiverse repos

--- scripts/programs/virtualbox/addppa.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-addvirtualboxkey () {
-  log "Add virtualbox keys..."
-  wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
-  wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
-}
-
-addrepo vbox "deb https://download.virtualbox.org/virtualbox/debian $(codename) contrib" addvirtualboxkey
