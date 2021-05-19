89502e6ce8a41a54bd6b2f1b8d8c5e503a4c16bf
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-05-19 14:55:12 -0700

    Add more info to home.nix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -2,12 +2,20 @@
 
 {
   imports = [
+    # CLI
     ./modules/bash.nix
     ./modules/zsh.nix
   ];
 
   programs.home-manager.enable = true;
 
-  home.packages = with pkgs; [ bash ];
-
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "21.05";
 }
