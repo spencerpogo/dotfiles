7876536a2733495ab46bc8440eee96a0a35b20be
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-05-20 10:29:22 -0700

    Remove debian_chroot reference

--- home-manager/modules/bash.nix
+++ home-manager/modules/bash.nix
@@ -36,7 +36,7 @@
       # If this is an xterm set the title to user@host:dir
       case "$TERM" in
       xterm*|rxvt*)
-          PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
+          PS1="\[\e]0;\u@\h: \w\a\]$PS1"
           ;;
       *)
           ;;
