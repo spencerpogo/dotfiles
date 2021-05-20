7e3ae3216e4bc621be3bee7f966b06c670a2604a
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-05-20 10:46:20 -0700

    Remove docker from host-specific config file

--- machines/redbox/configuration.nix
+++ machines/redbox/configuration.nix
@@ -99,9 +99,6 @@
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
 
-  # Enable Docker
-  virtualisation.docker.enable = true;
-
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
