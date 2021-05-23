a1a9cd6d2043fa6f1401caf3c8017e0a4d1fc7c5
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-05-23 13:57:35 -0700

    Run nixfmt

--- home-manager/modules/vscode.nix
+++ home-manager/modules/vscode.nix
@@ -1,9 +1,8 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [
-    (nerdfonts.override { fonts = [ "FiraCode" "Meslo" ]; })
-  ];
+  home.packages = with pkgs;
+    [ (nerdfonts.override { fonts = [ "FiraCode" "Meslo" ]; }) ];
 
   programs.vscode = {
     enable = true;
@@ -14,134 +13,134 @@
       WakaTime.vscode-wakatime
       ms-python.vscode-pylance
     ]) ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-    {
-      name = "Nix";
-      publisher = "bbenoist";
-      version = "1.0.1";
-      sha256 = "0zd0n9f5z1f0ckzfjr38xw2zzmcxg1gjrava7yahg5cvdcw6l35b";
-    }
-    {
-      name = "better-toml";
-      publisher = "bungcip";
-      version = "0.3.2";
-      sha256 = "08lhzhrn6p0xwi0hcyp6lj9bvpfj87vr99klzsiy8ji7621dzql3";
-    }
-    {
-      name = "bracket-pair-colorizer-2";
-      publisher = "CoenraadS";
-      version = "0.2.0";
-      sha256 = "0nppgfbmw0d089rka9cqs3sbd5260dhhiipmjfga3nar9vp87slh";
-    }
-    {
-      name = "vscode-eslint";
-      publisher = "dbaeumer";
-      version = "2.1.20";
-      sha256 = "0xk8pxv5jy89fshda845iswryvmgv0yxr11xsdbvd9zdczqhg7wc";
-    }
-    {
-      name = "gitlens";
-      publisher = "eamodio";
-      version = "11.4.1";
-      sha256 = "1gqkcv210h595rxwqsgwywjg2c2xnxdjlpqmgg091hd36g7jhcrs";
-    }
-    {
-      name = "EditorConfig";
-      publisher = "EditorConfig";
-      version = "0.16.4";
-      sha256 = "0fa4h9hk1xq6j3zfxvf483sbb4bd17fjl5cdm3rll7z9kaigdqwg";
-    }
-    {
-      name = "andromeda";
-      publisher = "EliverLara";
-      version = "1.7.0";
-      sha256 = "1899w5frankzg4g5va1cjv34gw9cdaxb3wdxqgzp694nffhzxb26";
-    }
-    {
-      name = "prettier-vscode";
-      publisher = "esbenp";
-      version = "6.4.0";
-      sha256 = "0k5x5d5axbpgyiy6j7q7d2rgxz6mg60sc0qgcca481srnca4j7x4";
-    }
-    {
-      name = "go";
-      publisher = "golang";
-      version = "0.25.0";
-      sha256 = "1g73bghi8knnvchkhdynvai034pnb95krks2xzhdhdac07dj227b";
-    }
-    {
-      name = "vscode-graphql";
-      publisher = "GraphQL";
-      version = "0.3.16";
-      sha256 = "120d5v0qpd1hhhz17xp0pgrmri13gdccz1xhcrn9pvbrxvsvp1il";
-    }
-    {
-      name = "discord-vscode";
-      publisher = "icrawl";
-      version = "5.6.3";
-      sha256 = "0z7niql0ax31mm03rkdknzm90s7ayqpz474cqx482iwdyi68ccfh";
-    }
-    {
-      name = "import-sorter";
-      publisher = "mike-co";
-      version = "3.3.1";
-      sha256 = "0z6biw0a06as63nb0pbxpk5j3dxwk7n1v14f4dk7f9hh7j69qp2s";
-    }
-    {
-      name = "processing-formatter";
-      publisher = "millennIumAMbiguity";
-      version = "0.4.4";
-      sha256 = "0fksfpf9b8x4mbl2d69hpii1105751s62dfmg30bv48aiabh6278";
-    }
-    {
-      name = "python";
-      publisher = "ms-python";
-      version = "2021.5.842923320";
-      sha256 = "183ram995n9dqg7d9g3bn30a1mg7nkkg4knr814f4j9lqzsai22r";
-    }
-    {
-      name = "cpptools";
-      publisher = "ms-vscode";
-      version = "1.3.1";
-      sha256 = "1qjvc2xkydgs606k94dfzs0m3lplrgwxln5rnngjrhgk9pxll945";
-    }
-    {
-      name = "vsliveshare";
-      publisher = "ms-vsliveshare";
-      version = "1.0.4272";
-      sha256 = "0dhs9lkprz2q79kpkj2d22d09lvhl77n38vj0rsz80g9vj41jzwh";
-    }
-    {
-      name = "material-icon-theme";
-      publisher = "PKief";
-      version = "4.6.0";
-      sha256 = "0jid21l8mdh0bism7yl0awkbj9802fb880rkpnva43p61npybvcb";
-    }
-    {
-      name = "LiveServer";
-      publisher = "ritwickdey";
-      version = "5.6.1";
-      sha256 = "077arf3hsn1yb8xdhlrax5gf93ljww78irv4gm8ffmsqvcr1kws0";
-    }
-    {
-      name = "language-pde";
-      publisher = "Tobiah";
-      version = "1.4.6";
-      sha256 = "1v2gwwllk269hba2mc22scfr0bizwndgg7zm9wsxffm52pwfjqp9";
-    }
-    {
-      name = "vim";
-      publisher = "vscodevim";
-      version = "1.20.3";
-      sha256 = "0za138wvp60dhz9abb0j4ida8jk7mzzpj8wga9ihc1cfxp8ad8an";
-    }
-    {
-      name = "vscode-proto3";
-      publisher = "zxh404";
-      version = "0.5.4";
-      sha256 = "08dfl5h1k6s542qw5qx2czm1wb37ck9w2vpjz44kp2az352nmksb";
-    }
-  ];
-  
+      {
+        name = "Nix";
+        publisher = "bbenoist";
+        version = "1.0.1";
+        sha256 = "0zd0n9f5z1f0ckzfjr38xw2zzmcxg1gjrava7yahg5cvdcw6l35b";
+      }
+      {
+        name = "better-toml";
+        publisher = "bungcip";
+        version = "0.3.2";
+        sha256 = "08lhzhrn6p0xwi0hcyp6lj9bvpfj87vr99klzsiy8ji7621dzql3";
+      }
+      {
+        name = "bracket-pair-colorizer-2";
+        publisher = "CoenraadS";
+        version = "0.2.0";
+        sha256 = "0nppgfbmw0d089rka9cqs3sbd5260dhhiipmjfga3nar9vp87slh";
+      }
+      {
+        name = "vscode-eslint";
+        publisher = "dbaeumer";
+        version = "2.1.20";
+        sha256 = "0xk8pxv5jy89fshda845iswryvmgv0yxr11xsdbvd9zdczqhg7wc";
+      }
+      {
+        name = "gitlens";
+        publisher = "eamodio";
+        version = "11.4.1";
+        sha256 = "1gqkcv210h595rxwqsgwywjg2c2xnxdjlpqmgg091hd36g7jhcrs";
+      }
+      {
+        name = "EditorConfig";
+        publisher = "EditorConfig";
+        version = "0.16.4";
+        sha256 = "0fa4h9hk1xq6j3zfxvf483sbb4bd17fjl5cdm3rll7z9kaigdqwg";
+      }
+      {
+        name = "andromeda";
+        publisher = "EliverLara";
+        version = "1.7.0";
+        sha256 = "1899w5frankzg4g5va1cjv34gw9cdaxb3wdxqgzp694nffhzxb26";
+      }
+      {
+        name = "prettier-vscode";
+        publisher = "esbenp";
+        version = "6.4.0";
+        sha256 = "0k5x5d5axbpgyiy6j7q7d2rgxz6mg60sc0qgcca481srnca4j7x4";
+      }
+      {
+        name = "go";
+        publisher = "golang";
+        version = "0.25.0";
+        sha256 = "1g73bghi8knnvchkhdynvai034pnb95krks2xzhdhdac07dj227b";
+      }
+      {
+        name = "vscode-graphql";
+        publisher = "GraphQL";
+        version = "0.3.16";
+        sha256 = "120d5v0qpd1hhhz17xp0pgrmri13gdccz1xhcrn9pvbrxvsvp1il";
+      }
+      {
+        name = "discord-vscode";
+        publisher = "icrawl";
+        version = "5.6.3";
+        sha256 = "0z7niql0ax31mm03rkdknzm90s7ayqpz474cqx482iwdyi68ccfh";
+      }
+      {
+        name = "import-sorter";
+        publisher = "mike-co";
+        version = "3.3.1";
+        sha256 = "0z6biw0a06as63nb0pbxpk5j3dxwk7n1v14f4dk7f9hh7j69qp2s";
+      }
+      {
+        name = "processing-formatter";
+        publisher = "millennIumAMbiguity";
+        version = "0.4.4";
+        sha256 = "0fksfpf9b8x4mbl2d69hpii1105751s62dfmg30bv48aiabh6278";
+      }
+      {
+        name = "python";
+        publisher = "ms-python";
+        version = "2021.5.842923320";
+        sha256 = "183ram995n9dqg7d9g3bn30a1mg7nkkg4knr814f4j9lqzsai22r";
+      }
+      {
+        name = "cpptools";
+        publisher = "ms-vscode";
+        version = "1.3.1";
+        sha256 = "1qjvc2xkydgs606k94dfzs0m3lplrgwxln5rnngjrhgk9pxll945";
+      }
+      {
+        name = "vsliveshare";
+        publisher = "ms-vsliveshare";
+        version = "1.0.4272";
+        sha256 = "0dhs9lkprz2q79kpkj2d22d09lvhl77n38vj0rsz80g9vj41jzwh";
+      }
+      {
+        name = "material-icon-theme";
+        publisher = "PKief";
+        version = "4.6.0";
+        sha256 = "0jid21l8mdh0bism7yl0awkbj9802fb880rkpnva43p61npybvcb";
+      }
+      {
+        name = "LiveServer";
+        publisher = "ritwickdey";
+        version = "5.6.1";
+        sha256 = "077arf3hsn1yb8xdhlrax5gf93ljww78irv4gm8ffmsqvcr1kws0";
+      }
+      {
+        name = "language-pde";
+        publisher = "Tobiah";
+        version = "1.4.6";
+        sha256 = "1v2gwwllk269hba2mc22scfr0bizwndgg7zm9wsxffm52pwfjqp9";
+      }
+      {
+        name = "vim";
+        publisher = "vscodevim";
+        version = "1.20.3";
+        sha256 = "0za138wvp60dhz9abb0j4ida8jk7mzzpj8wga9ihc1cfxp8ad8an";
+      }
+      {
+        name = "vscode-proto3";
+        publisher = "zxh404";
+        version = "0.5.4";
+        sha256 = "08dfl5h1k6s542qw5qx2czm1wb37ck9w2vpjz44kp2az352nmksb";
+      }
+    ];
+
     keybindings = [
       {
         key = "ctrl+space";
@@ -189,9 +188,7 @@
       };
       "[rust]"."editor.tabSize" = 4;
       "[python]" = {
-        "gitlens.codeLens.symbolScopes" = [
-          "!Module"
-        ];
+        "gitlens.codeLens.symbolScopes" = [ "!Module" ];
         "editor.wordBasedSuggestions" = false;
         "editor.tabSize" = 4;
       };
@@ -233,10 +230,8 @@
       "java.semanticHighlighting.enabled" = true;
       "keyboard.dispatch" = "keyCode";
       "python.analysis.logLevel" = "Trace";
-      "python.autoComplete.extraPaths" = [
-        "."
-        "/home/scoder12/.pyenv/versions/3.8.2/lib/python3.8"
-      ];
+      "python.autoComplete.extraPaths" =
+        [ "." "/home/scoder12/.pyenv/versions/3.8.2/lib/python3.8" ];
       "python.formatting.provider" = "black";
       "python.languageServer" = "Microsoft";
       "terminal.integrated.fontFamily" = "MesloLGS NF";
