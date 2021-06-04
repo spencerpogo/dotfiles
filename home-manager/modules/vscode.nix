65710190d786f16ce2098fa9ffce215300e078fc
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-06-03 18:31:34 -0700

    Use pylance as language server

--- home-manager/modules/vscode.nix
+++ home-manager/modules/vscode.nix
@@ -213,7 +213,7 @@
       "python.autoComplete.extraPaths" =
         [ "." "/home/scoder12/.pyenv/versions/3.8.2/lib/python3.8" ];
       "python.formatting.provider" = "black";
-      "python.languageServer" = "Microsoft";
+      "python.languageServer": "Pylance";
       "terminal.integrated.fontFamily" = "MesloLGS NF";
       "vim.handleKeys" = {
         "<C-a>" = false;
