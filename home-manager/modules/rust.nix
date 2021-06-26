36dabb1e036ad905e03033719b02e0e01a91dfe4
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-06-26 16:04:25 -0700

    Add rustfmt

--- home-manager/modules/rust.nix
+++ home-manager/modules/rust.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  home.packages = with pkgs; [ cargo rustc ];
+  home.packages = with pkgs; [ cargo rustc rustfmt ];
 
   home.sessionPath = [ "${config.home.homeDirectory}/.cargo/bin" ];
 }
