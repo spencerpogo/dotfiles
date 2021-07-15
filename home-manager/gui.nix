31c458662cbd3faf684cff119023b4d347e43deb
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-07-15 16:50:15 -0700

    Move GUI unfree predicates to gui.nix

--- home-manager/gui.nix
+++ home-manager/gui.nix
@@ -15,4 +15,16 @@
   fonts.fontconfig.enable = true;
 
   home.keyboard.options = [ "caps:escape_shifted_capslock" ];
+
+  nixpkgs.config.allowUnfreePredicate = pkg:
+    builtins.elem (lib.getName pkg) [
+      "vscode-extension-MS-python-vscode-pylance"
+      "vscode-extension-ms-vscode-cpptools"
+      "vscode-extension-ms-toolsai-jupyter"
+      "vscode-extension-ms-vsliveshare-vsliveshare"
+      "steam"
+      "steam-original"
+      "steam-runtime"
+      "obsidian"
+    ];
 }
