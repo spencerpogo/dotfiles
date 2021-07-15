a6eafa7385efa33c410ab151e40049305b1bccb0
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-07-15 14:08:08 -0700

    Use nixpkgs provided extensions and disable some
    nixpkgs provided extensions get updated once in a while, while manual ones don't.

--- home-manager/modules/vscode.nix
+++ home-manager/modules/vscode.nix
@@ -20,42 +20,23 @@
       ms-vsliveshare.vsliveshare
       ms-toolsai.jupyter
       vscodevim.vim
+      bbenoist.Nix
+      coenraads.bracket-pair-colorizer-2
+      dbaeumer.vscode-eslint
+      eamodio.gitlens
+      editorconfig.editorconfig
+      esbenp.prettier-vscode
+      golang.Go
+      graphql.vscode-graphql
+      pkief.material-icon-theme
+      haskell.haskell
+      justusadam.language-haskell
     ]) ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-      {
-        name = "Nix";
-        publisher = "bbenoist";
-        version = "1.0.1";
-        sha256 = "0zd0n9f5z1f0ckzfjr38xw2zzmcxg1gjrava7yahg5cvdcw6l35b";
-      }
       {
         name = "better-toml";
         publisher = "bungcip";
         version = "0.3.2";
         sha256 = "08lhzhrn6p0xwi0hcyp6lj9bvpfj87vr99klzsiy8ji7621dzql3";
-      }
-      {
-        name = "bracket-pair-colorizer-2";
-        publisher = "CoenraadS";
-        version = "0.2.0";
-        sha256 = "0nppgfbmw0d089rka9cqs3sbd5260dhhiipmjfga3nar9vp87slh";
-      }
-      {
-        name = "vscode-eslint";
-        publisher = "dbaeumer";
-        version = "2.1.20";
-        sha256 = "0xk8pxv5jy89fshda845iswryvmgv0yxr11xsdbvd9zdczqhg7wc";
-      }
-      {
-        name = "gitlens";
-        publisher = "eamodio";
-        version = "11.4.1";
-        sha256 = "1gqkcv210h595rxwqsgwywjg2c2xnxdjlpqmgg091hd36g7jhcrs";
-      }
-      {
-        name = "EditorConfig";
-        publisher = "EditorConfig";
-        version = "0.16.4";
-        sha256 = "0fa4h9hk1xq6j3zfxvf483sbb4bd17fjl5cdm3rll7z9kaigdqwg";
       }
       {
         name = "andromeda";
@@ -64,30 +45,12 @@
         sha256 = "1899w5frankzg4g5va1cjv34gw9cdaxb3wdxqgzp694nffhzxb26";
       }
       {
-        name = "prettier-vscode";
-        publisher = "esbenp";
-        version = "6.4.0";
-        sha256 = "0k5x5d5axbpgyiy6j7q7d2rgxz6mg60sc0qgcca481srnca4j7x4";
-      }
-      {
-        name = "go";
-        publisher = "golang";
-        version = "0.25.0";
-        sha256 = "1g73bghi8knnvchkhdynvai034pnb95krks2xzhdhdac07dj227b";
-      }
-      {
-        name = "vscode-graphql";
-        publisher = "GraphQL";
-        version = "0.3.16";
-        sha256 = "120d5v0qpd1hhhz17xp0pgrmri13gdccz1xhcrn9pvbrxvsvp1il";
-      }
-      {
         name = "discord-vscode";
         publisher = "icrawl";
         version = "5.6.3";
         sha256 = "0z7niql0ax31mm03rkdknzm90s7ayqpz474cqx482iwdyi68ccfh";
       }
-      {
+      /*{
         name = "import-sorter";
         publisher = "mike-co";
         version = "3.3.1";
@@ -98,12 +61,6 @@
         publisher = "millennIumAMbiguity";
         version = "0.4.4";
         sha256 = "0fksfpf9b8x4mbl2d69hpii1105751s62dfmg30bv48aiabh6278";
-      }
-      {
-        name = "material-icon-theme";
-        publisher = "PKief";
-        version = "4.6.0";
-        sha256 = "0jid21l8mdh0bism7yl0awkbj9802fb880rkpnva43p61npybvcb";
       }
       {
         name = "LiveServer";
@@ -122,19 +79,7 @@
         publisher = "zxh404";
         version = "0.5.4";
         sha256 = "08dfl5h1k6s542qw5qx2czm1wb37ck9w2vpjz44kp2az352nmksb";
-      }
-      {
-        name = "haskell";
-        publisher = "haskell";
-        version = "1.4.0";
-        sha256 = "1jk702fd0b0aqfryixpiy6sc8njzd1brd0lbkdhcifp0qlbdwki0";
-      }
-      {
-        name = "language-haskell";
-        publisher = "justusadam";
-        version = "3.4.0";
-        sha256 = "0ab7m5jzxakjxaiwmg0jcck53vnn183589bbxh3iiylkpicrv67y";
-      }
+      }*/
     ];
 
     keybindings = [
