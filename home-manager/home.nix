dc34732212b2f89f64cc0dd5953fa65bcefa589a
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-07-15 16:53:07 -0700

    Remove duplicate unfree predicates

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -7,16 +7,4 @@
 
   # FIXME non-nixos only
   targets.genericLinux.enable = true;
-
-  nixpkgs.config.allowUnfreePredicate = pkg:
-    builtins.elem (lib.getName pkg) [
-      "vscode-extension-MS-python-vscode-pylance"
-      "vscode-extension-ms-vscode-cpptools"
-      "vscode-extension-ms-toolsai-jupyter"
-      "vscode-extension-ms-vsliveshare-vsliveshare"
-      "steam"
-      "steam-original"
-      "steam-runtime"
-      "obsidian"
-    ];
 }
