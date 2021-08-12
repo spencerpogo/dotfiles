833f735db92fb1b3ccb94af7db87afdd43ab8583
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-08-12 16:08:19 -0700

    Refactor unfree predicate into base.nix

---
+++ home-manager/base.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }:
+
+{
+  nixpkgs.config.allowUnfreePredicate = pkg:
+    builtins.elem (lib.getName pkg) [
+      "vscode-extension-MS-python-vscode-pylance"
+      "vscode-extension-ms-vscode-cpptools"
+      "vscode-extension-ms-toolsai-jupyter"
+      "vscode-extension-ms-vsliveshare-vsliveshare"
+      "steam"
+      "steam-original"
+      "steam-runtime"
+      "obsidian"
+      "discord"
+    ];
+}
--- home-manager/gui.nix
+++ home-manager/gui.nix
@@ -8,17 +8,4 @@
     # GNOME specific packages and settings
     ./modules/gnome.nix
   ];
-
-  nixpkgs.config.allowUnfreePredicate = pkg:
-    builtins.elem (lib.getName pkg) [
-      "vscode-extension-MS-python-vscode-pylance"
-      "vscode-extension-ms-vscode-cpptools"
-      "vscode-extension-ms-toolsai-jupyter"
-      "vscode-extension-ms-vsliveshare-vsliveshare"
-      "steam"
-      "steam-original"
-      "steam-runtime"
-      "obsidian"
-      "discord"
-    ];
 }
