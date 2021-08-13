b9a0a63a593609fcb15b2f2e32d87dbd43a380f5
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-08-12 17:09:46 -0700

    Run nixfmt

--- home-manager/modules/guiutils.nix
+++ home-manager/modules/guiutils.nix
@@ -2,10 +2,11 @@
 
 # Utitilities that are only useful on a GUI system
 {
-  home.packages = with pkgs; [
-    # for copying from command line. extremely useful
-    xclip
-  ];
+  home.packages = with pkgs;
+    [
+      # for copying from command line. extremely useful
+      xclip
+    ];
 
   # enable nix managaing fonts (some GUI modules such as vscode and gnome-terminal need
   #  them)
