a2d79008d400793ad1d221f22ecb0dd7a53a2c1a
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-08-12 17:09:58 -0700

    Add parrot profile

---
+++ home-manager/parrot.nix
@@ -0,0 +1,14 @@
+{ pkgs, ... }:
+
+# My VM used for pentesting/CTF playing running parrot linux (because
+#  that is what is cool these days *eyeroll*)
+{
+  imports = [
+    ./modules/base.nix
+    ./modules/cli.nix
+    ./modules/python.nix
+    ./modules/vscode.nix
+    ./modules/firefox.nix
+    ./modules/guiutils.nix
+  ];
+}
