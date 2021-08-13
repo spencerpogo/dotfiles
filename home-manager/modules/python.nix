5c41ce020f4050ff926adf628b7ea33e8915956f
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-08-12 17:01:16 -0700

    Fix parse error

--- home-manager/modules/python.nix
+++ home-manager/modules/python.nix
@@ -2,8 +2,5 @@
 
 # I always need python available offline. Essential for scripting.
 {
-  home.packages = with pkgs.python39Packages; [
-    python.withPackages
-    (ppkgs: [ ppkgs.black ])
-  ];
+  home.packages = [ (pkgs.python39.withPackages (ppkgs: [ ppkgs.black ])) ];
 }
