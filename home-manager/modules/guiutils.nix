9141fad33b0b839844ff5c964cde7a5a67cafa3b
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-10-30 17:37:02 -0700

    Remove keyboard options
    They are set at system level

--- home-manager/modules/guiutils.nix
+++ home-manager/modules/guiutils.nix
@@ -11,7 +11,4 @@
   # enable nix managaing fonts (some GUI modules such as vscode and gnome-terminal need
   #  them)
   fonts.fontconfig.enable = true;
-
-  # TODO: remove, useless
-  home.keyboard.options = [ "caps:escape_shifted_capslock" ];
 }
