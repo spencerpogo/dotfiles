1be7fbe2552a5c157390394ac58706f1abec2386
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-10-30 17:06:16 -0700

    Install file command globally

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -121,6 +121,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     curl
     wget
+    file
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
