41505b63481c2a68f8015a3cf3a332e56a79e4cf
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-11-03 18:05:25 -0700

    Add proper chinese fonts

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -56,6 +56,8 @@
     enabled = "ibus";
     ibus.engines = [ pkgs.ibus-engines.libpinyin ];
   };
+
+  fonts.fonts = [ pkgs.noto-fonts-cjk ];
   # console = {
   #   font = "Lat2-Terminus16";
   #   keyMap = "us";
