259ce9e55e8dd8118759dd13e334a1759ce1a035
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-11-09 17:00:13 -0800

    WIP: Name i3 workspaces

--- home-manager/modules/i3.nix
+++ home-manager/modules/i3.nix
@@ -1,18 +1,35 @@
 { config, lib, pkgs, ... }:
 
-let mod = "Mod4";
+let
+  mod = "Mod4";
+  esc = v: ''"${v}"'';
+  ws1 = esc "1:one";
+  ws2 = esc "2:two";
+  ws3 = esc "3";
+  ws4 = esc "4";
+  ws5 = esc "5";
+  ws6 = esc "6";
+  ws7 = esc "7";
+  ws8 = esc "8";
+  ws9 = esc "9";
+  ws10 = esc "10";
 in {
   # Start i3 from home-manager
   # https://discourse.nixos.org/t/opening-i3-from-home-manager-automatically/4849/8
   xsession = {
     enable = true;
     scriptPath = ".hm-xsession";
-    # https://github.com/flameshot-org/flameshot/issues/168#issuecomment-377851744
-    windowManager.command = lib.mkForce
-      "${pkgs.dbus}/bin/dbus-launch ${config.xsession.windowManager.i3.package}/bin/i3";
   };
 
-  home.packages = [ pkgs.dmenu pkgs.i3status pkgs.i3lock pkgs.feh ];
+  home.packages = [
+    pkgs.i3
+    pkgs.dmenu
+    pkgs.i3status
+    pkgs.i3lock
+    pkgs.feh
+    pkgs.xorg.xrandr
+    pkgs.flameshot
+  ];
 
   xsession.windowManager.i3 = {
     enable = true;
@@ -21,6 +38,28 @@ in {
       keybindings = lib.mkOptionDefault {
         "${mod}+n" = "move workspace to output left";
         "${mod}+m" = "move workspace to output right";
+
+        "${mod}+1" = "workspace number ${ws1}";
+        "${mod}+2" = "workspace number ${ws2}";
+        "${mod}+3" = "workspace number ${ws3}";
+        "${mod}+4" = "workspace number ${ws4}";
+        "${mod}+5" = "workspace number ${ws5}";
+        "${mod}+6" = "workspace number ${ws6}";
+        "${mod}+7" = "workspace number ${ws7}";
+        "${mod}+8" = "workspace number ${ws8}";
+        "${mod}+9" = "workspace number ${ws9}";
+        "${mod}+0" = "workspace number ${ws10}";
+
+        "${mod}+Shift+1" = "move container to workspace number ${ws1}";
+        "${mod}+Shift+2" = "move container to workspace number ${ws2}";
+        "${mod}+Shift+3" = "move container to workspace number ${ws3}";
+        "${mod}+Shift+4" = "move container to workspace number ${ws4}";
+        "${mod}+Shift+5" = "move container to workspace number ${ws5}";
+        "${mod}+Shift+6" = "move container to workspace number ${ws6}";
+        "${mod}+Shift+7" = "move container to workspace number ${ws7}";
+        "${mod}+Shift+8" = "move container to workspace number ${ws8}";
+        "${mod}+Shift+9" = "move container to workspace number ${ws9}";
+        "${mod}+Shift+0" = "move container to workspace number ${ws9}";
 
         # vim keybindings
         "${mod}+h" = "focus left";
@@ -47,9 +86,7 @@ in {
           command =
             "${pkgs.feh}/bin/feh --bg-fill ${pkgs.nixos-artwork.wallpapers.simple-dark-gray-bottom.gnomeFilePath}";
         }
-        {
-          command = "${pkgs.flameshot}/bin/flameshot";
-        }
+        { command = "${pkgs.flameshot}/bin/flameshot"; }
       ];
       window.hideEdgeBorders = "both";
       workspaceAutoBackAndForth = true;
@@ -58,5 +95,11 @@ in {
       else
         "i3-sensible-terminal";
     };
+    extraConfig = ''
+      strip_workspace_numbers yes
+      # Set default workspaces
+      exec --no-startup-id i3-msg workspace ${ws1}
+      exec --no-startup-id i3-msg workspace ${ws2}
+    '';
   };
 }
