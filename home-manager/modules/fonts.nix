8bad2327ad52c49743689409f4026624e1a51985
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-11-11 21:53:21 -0800

    Increase icon size

--- home-manager/modules/fonts.nix
+++ home-manager/modules/fonts.nix
@@ -16,4 +16,29 @@
     })
     pkgs.font-awesome_5
   ];
+
+  xdg.configFile."fontconfig/conf.d/80-scale-fontawesome.conf".text = ''
+    <?xml version='1.0'?>
+    <!-- Increase FontAwesome icons size. A hacky way to make i3 workspace icons bigger. -->
+    <!DOCTYPE fontconfig SYSTEM 'fonts.dtd'>
+    <fontconfig>
+      <!-- https://unix.stackexchange.com/a/41110/284442 -->
+      <match target="font">
+          <test name="family">
+              <string>Font Awesome 5 Free</string>
+          </test>
+          <edit name="pixelsize" mode="assign">
+              <times><name>pixelsize</name>, <double>1.5</double></times>
+          </edit>
+      </match>
+      <match target="font">
+          <test name="family">
+              <string>Font Awesome 5 Brands</string>
+          </test>
+          <edit name="pixelsize" mode="assign">
+              <times><name>pixelsize</name>, <double>1.5</double></times>
+          </edit>
+      </match>
+    </fontconfig>
+  '';
 }
