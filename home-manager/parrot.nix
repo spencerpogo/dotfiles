f7fa74480b10c6f701e5b14e773221faec21537e
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-11-26 02:49:29 +0000

    Update parrot profile

--- home-manager/parrot.nix
+++ home-manager/parrot.nix
@@ -6,13 +6,17 @@
   imports = [
     ./modules/base.nix
     ./modules/cli.nix
+    ./modules/rust.nix
     ./modules/python.nix
     ./modules/vscode.nix
     ./modules/firefox.nix
     ./modules/guiutils.nix
+    # TODO: test this and add to cli.nix or parrot.nix
     ./modules/tmux.nix
-    ./modules/gnome-terminal.nix
     ./modules/pentesting.nix
+    ./modules/flameshot.nix
+    ./modules/fonts.nix
+    ./modules/alacritty.nix
   ];
 
   # enable non-nixos
