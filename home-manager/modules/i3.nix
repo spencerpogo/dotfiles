6b6a360612614198d1d8cac943301813c2f42c1a
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-11-28 18:26:45 -0800

    Setup i3status-rust

--- home-manager/modules/i3.nix
+++ home-manager/modules/i3.nix
@@ -32,6 +32,47 @@ in {
     pkgs.flameshot
   ];
 
+  programs.i3status-rust = {
+    enable = true;
+    bars.bottom = {
+      theme = "nord-dark";
+      icons = "awesome5";
+      blocks = [
+        {
+          block = "disk_space";
+          path = "/";
+          alias = "/";
+          info_type = "available";
+          unit = "GB";
+          interval = 15;
+          warning = 20.0;
+          alert = 10.0;
+        }
+        {
+          block = "memory";
+          display_type = "memory";
+          format_mem = "{mem_used_percents}";
+          format_swap = "{swap_used_percents}";
+        }
+        {
+          block = "cpu";
+          interval = 1;
+        }
+        {
+          block = "load";
+          interval = 1;
+          format = "{1m}";
+        }
+        { block = "sound"; }
+        {
+          block = "time";
+          interval = 1;
+          format = "%a %m/%d %R";
+        }
+      ];
+    };
+  };
+
   xsession.windowManager.i3 = {
     enable = true;
     config = {
@@ -136,7 +177,8 @@ in {
         position = "bottom";
         workspaceButtons = true;
         workspaceNumbers = false;
-        statusCommand = "${pkgs.i3status}/bin/i3status";
+        statusCommand =
+          "${config.programs.i3status-rust.package}/bin/i3status-rs ${config.xdg.configHome}/i3status-rust/config-bottom.toml";
         fonts = {
           # Monospace still makes fontwawesome fonts work
           # putting fontawesome as the font face makes colons off-center
@@ -145,7 +187,7 @@ in {
         };
         trayOutput = "primary";
         colors = {
-          background = "#000000";
+          background = "#2e3440";
           statusline = "#ffffff";
           separator = "#666666";
           focusedWorkspace = {
