2d97dfb5811578fd350b2d49de0891008b591ace
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-11-28 19:52:55 -0800

    Revert back to fcitx
    This reverts commit 80e402949badef1f60dd5aab3df996bdc5f7341f.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -53,8 +53,8 @@
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.inputMethod = {
-    enabled = "ibus";
-    ibus.engines = [ pkgs.ibus-engines.libpinyin ];
+    enabled = "fcitx";
+    fcitx.engines = [ pkgs.fcitx-engines.libpinyin ];
   };
 
   fonts.fonts = [ pkgs.noto-fonts-cjk ];
@@ -129,7 +129,7 @@
     curl
     wget
     file
-    ibus
+    fcitx
     psmisc # fuser, killall and pstree
   ];
 
