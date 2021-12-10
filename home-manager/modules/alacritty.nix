b4c347e9e8c2418e71221c57299572607e4bb9e6
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-12-09 20:37:08 -0800

    Configure alacritty palette

--- home-manager/modules/alacritty.nix
+++ home-manager/modules/alacritty.nix
@@ -12,14 +12,43 @@ in {
         bold_italic = termfont;
         size = "12";
       };
+
       cursor = {
         style = "Beam";
         thickness = 0.2;
       };
+
       shell.program = if config.programs.tmux.enable then
         "${pkgs.tmux}/bin/tmux"
       else
         config.home.sessionVariables.SHELL;
+
+      colors.selection = {
+        text = "#ffffff";
+        background = "#48b9c7";
+      };
+
+      colors.normal = {
+        black = "#333333";
+        red = "#cc0000";
+        green = "#4e9a06";
+        yellow = "#c4a000";
+        blue = "#3465a4";
+        magenta = "#75507b";
+        cyan = "#06989a";
+        white = "#d3d7cf";
+      };
+
+      colors.bright = {
+        black = "#88807c";
+        red = "#f15d22";
+        green = "#73c48f";
+        yellow = "#ffce51";
+        blue = "#48b9c7";
+        magenta = "#ad7fa8";
+        cyan = "#34e2e2";
+        white = "#eeeeec";
+      };
     };
   };
 }
