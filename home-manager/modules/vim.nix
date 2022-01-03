13b297cd71d161ec4199854e533835b929093b53
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2022-01-02 18:12:25 -0800

    Change vim cursor based on mode

--- home-manager/modules/vim.nix
+++ home-manager/modules/vim.nix
@@ -1,1 +1,11 @@
-{ ... }: { programs.vim = { enable = true; }; }
+{ ... }: {
+  programs.vim = {
+    enable = true;
+    extraConfig = ''
+      " Escape sequence on enter normal mode
+      let &t_SI = "\e[6 q"
+      " Escape sequence on enter insert mode
+      let &t_EI = "\e[2 q"
+    '';
+  };
+}
