ae2eb6f94592ef5aaa86cc1a4f762e7b37118f24
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2022-02-18 18:35:34 -0800

    Update file organization

--- home-manager/modules/cli.nix
+++ home-manager/modules/cli.nix
@@ -2,7 +2,7 @@
 
 # Minimal CLI setup: shells and a couple essential tools.
 {
-  imports = [ ./bash.nix ./zsh.nix ./git.nix ];
+  imports = [ ./bash.nix ./zsh/zsh.nix ./git.nix ];
 
   # Shell tools
   home.packages = with pkgs; [ ripgrep nixfmt ];
