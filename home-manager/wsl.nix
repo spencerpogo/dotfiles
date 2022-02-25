acb92643088410612f40b44a36ffa745be23570a
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2022-02-24 19:43:21 -0800

    Add wsl home-manager profile

---
+++ home-manager/wsl.nix
@@ -0,0 +1,18 @@
+{ pkgs, lib, ... }:
+
+# Ubuntu WSL Machine
+{
+  imports = [ ./configs/base.nix ./configs/cli.nix ./modules/tmux.nix ];
+
+  home.sessionVariables = {
+    EDITOR = "vim";
+    SHELL = "${pkgs.zsh}/bin/zsh";
+  };
+
+  home.packages = [
+    pkgs.lazygit
+  ];
+
+  # enable non-nixos
+  targets.genericLinux.enable = true;
+}
