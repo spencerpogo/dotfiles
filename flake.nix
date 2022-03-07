d24eff128d690fa59d0a12efba1b00bbc933f269
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2022-03-06 16:39:36 -0800

    Setup home-manager as a flake

--- flake.nix
+++ flake.nix
@@ -1,10 +1,26 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-  outputs = { self, nixpkgs }: {
-    nixosConfigurations.redbox12 = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [ ./redbox12/configuration.nix ];
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
+  outputs = { self, nixpkgs, home-manager }:
+    let
+      system = "x86_64-linux";
+      username = "spencer";
+    in {
+      nixosConfigurations.redbox12 = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [ ./redbox12/configuration.nix ];
+      };
+      homeConfigurations.${username} = home-manager.lib.homeManagerConfiguration {
+        inherit system username;
+        configuration = import ./home-manager/redbox.nix;
+        homeDirectory = "/home/${username}";
+        stateVersion = "21.11";
+      };
+    };
 }
