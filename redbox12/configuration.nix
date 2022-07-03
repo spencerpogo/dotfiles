10fae7a44b75f957a73662fd1bc54834d653b610
Spencer Pogorzelski <34356756+Scoder12@users.noreply.github.com>
2022-07-03 14:25:47 -0700

    Create virtual home-manager channel

--- redbox12/configuration.nix
+++ redbox12/configuration.nix
@@ -80,7 +80,8 @@
   # Channel compat
   # https://ayats.org/blog/channels-to-flakes/
   environment.etc."nix/inputs/nixpkgs".source = inputs.nixpkgs.outPath;
-  nix.nixPath = [ "nixpkgs=/etc/nix/inputs/nixpkgs" ];
+  environment.etc."nix/inputs/home-manager".source = inputs.home-manager.outPath;
+  nix.nixPath = [ "nixpkgs=/etc/nix/inputs/nixpkgs" "home-manager=/etc/nix/inputs/home-manager" ];
   nix.registry = with lib;
     mapAttrs' (name: value: nameValuePair name { flake = value; }) inputs;
 
