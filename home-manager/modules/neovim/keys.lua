f16529ea69be4929611d36fc1a1274d0e194eb90
Spencer Pogorzelski <34356756+Scoder12@users.noreply.github.com>
2022-07-03 15:29:42 -0700

    neovim switch window keybinds

--- home-manager/modules/neovim/keys.lua
+++ home-manager/modules/neovim/keys.lua
@@ -8,3 +8,7 @@ function map(mode, lhs, rhs, opts)
 end
 
 map("n", "<C-n>", ":NvimTreeToggle<CR>")
+map("n", "<C-h>", "<C-w>h")
+map("n", "<C-l>", "<C-w>l")
+map("n", "<C-j>", "<C-w>j")
+map("n", "<C-k>", "<C-w>k")
