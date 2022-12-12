3343d43322f899addfd2f427bb0901fc305ff0b5
Spencer Pogorzelski <34356756+Scoder12@users.noreply.github.com>
2022-12-11 20:47:51 -0800

    zsh cleanup

--- home-manager/modules/zsh/zshrc.zsh
+++ home-manager/modules/zsh/zshrc.zsh
@@ -32,10 +32,12 @@ function zle-keymap-select {
   fi
 }
 zle -N zle-keymap-select
+
 zle-line-init() {
     zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
     echo -ne "\e[5 q"
 }
 zle -N zle-line-init
-echo -ne '\e[5 q' # Use beam shape cursor on startup.
-preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt.
+
+preexec() { echo -ne '\e[5 q' ; } # Use beam shape cursor for each new prompt.
+preexec # Use beam shape cursor on startup.
