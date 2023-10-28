989d8f3775f5f084e9ffdc9a597ac83e4bd58750
Spencer Pogorzelski <34356756+Scoder12@users.noreply.github.com>
2023-10-28 15:33:05 -0700

    remove comment

--- home-manager/modules/vscode.nix
+++ home-manager/modules/vscode.nix
@@ -30,7 +30,6 @@
         ms-vscode.cpptools
         # python (essential)
         # too stupid to pick up on nix-installed black sadly
-        # Currently broken https://github.com/NixOS/nixpkgs/issues/263493
         ms-python.python
         ms-vsliveshare.vsliveshare
         ms-toolsai.jupyter
