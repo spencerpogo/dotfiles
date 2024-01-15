72dc187f950e8bcc5655b2b13b9bd9db6e46869f
Spencer Pogorzelski <34356756+Scoder12@users.noreply.github.com>
2024-01-15 12:24:05 -0800

    fix renamed avahi option

--- nixos/redbox12/configuration.nix
+++ nixos/redbox12/configuration.nix
@@ -84,7 +84,7 @@
   services.printing.enable = true;
   services.printing.drivers = [ pkgs.gutenprint ];
   services.avahi.enable = true;
-  services.avahi.nssmdns = true;
+  services.avahi.nssmdns4 = true;
 
   # Enable sound.
   sound.enable = true;
