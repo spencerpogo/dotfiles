b109831f6f1fd8df47c9967af81081c557fe0054
Spencer Pogorzelski <34356756+Scoder12@users.noreply.github.com>
2024-06-09 17:28:57 -0700

    install black CLI properly

--- home-manager/modules/python.nix
+++ home-manager/modules/python.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }:
 # I always need python available offline. Essential for scripting.
 {
-  home.packages = [ (pkgs.python3.withPackages (ppkgs: [ ppkgs.black ])) ];
+  home.packages = [ pkgs.python3 pkgs.black ];
 }
