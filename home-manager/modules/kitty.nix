a87a4fd2f2f408ba860f63e85f8dca683bd96f62
Spencer Pogorzelski <34356756+spencerpogo@users.noreply.github.com>
2025-02-05 13:04:20 -0500

    further configure kitty

--- home-manager/modules/kitty.nix
+++ home-manager/modules/kitty.nix
@@ -3,6 +3,14 @@
   pkgs,
   ...
 }:
+
+let
+  shellConfig =
+    if config.programs.tmux.enable then
+      "${pkgs.runtimeShell} -c \"tmux attach || tmux new\""
+    else
+      config.home.sessionVariables.SHELL;
+in
 {
   imports = [ ./fonts.nix ];
 
@@ -13,5 +21,12 @@
       size = 11.5;
     };
     themeFile = "Doom_Vibrant";
+
+    extraConfig = ''
+      shell ${shellConfig}
+
+      # tighter line height (like alacritty)
+      modify_font cell_height 90%
+    '';
   };
 }
