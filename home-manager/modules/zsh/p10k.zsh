60d71a31080af387e6b0260e385d8c670e5a055c
Spencer Pogorzelski <34356756+spencerpogo@users.noreply.github.com>
2025-02-05 12:42:16 -0500

    increase precision of p10k execution time

--- home-manager/modules/zsh/p10k.zsh
+++ home-manager/modules/zsh/p10k.zsh
@@ -550,7 +550,7 @@
   # Show duration of the last command if takes at least this many seconds.
   typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_THRESHOLD=3
   # Show this many fractional digits. Zero means round to seconds.
-  typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_PRECISION=0
+  typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_PRECISION=2
   # Execution time color.
   typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_FOREGROUND=3
   # Duration format: 1d 2h 3m 4s.
