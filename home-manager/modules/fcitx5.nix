afebcf9461f2ca1766213f3b1eeccbc108f506ff
Spencer Pogorzelski <34356756+spencerpogo@users.noreply.github.com>
2025-10-25 13:41:45 -0400

    temp disable fcitx

--- home-manager/modules/fcitx5.nix
+++ home-manager/modules/fcitx5.nix
@@ -1,7 +1,8 @@
 { pkgs, ... }:
 {
   i18n.inputMethod = {
-    enable = true;
+    # https://github.com/NixOS/nixpkgs/issues/454887
+    # enable = true;
     type = "fcitx5";
     fcitx5.addons = [ pkgs.fcitx5-chinese-addons ];
   };
